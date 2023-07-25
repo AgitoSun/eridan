63e51f6438b5711f080fd40648239398079cf53e
AgitoSun <zeke1@mail.ru>
2023-07-25 13:11:30 +0300

    vite

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-                'resources/img',
+                'resources/img/guest/icons/arrow-right.svg',
             ],
             refresh: true,
         }),
